03e8d081c6076d531dd76710d944cba2dc46f499
Changming Liu <charley.ashbringer@gmail.com>
2022-04-23 21:00:34 -0400

    [orc]add arg_no parse for ddg edges

--- sym_backend/graph/RuntimeDataDepGraph.cpp
+++ sym_backend/graph/RuntimeDataDepGraph.cpp
@@ -10,13 +10,19 @@
 void RuntimeSymDepGraph::readGraphViz(std::string filename) {
     boost::dynamic_properties dp(boost::ignore_other_properties);
 
-    dp.property("node_id",           boost::get(&Vertex_Properties::name,  graph));
+    dp.property("node_id",           boost::get(&Vertex_Properties::name,         graph));
     dp.property(symIDPrefix,         boost::get(&Vertex_Properties::symID,        graph));
     dp.property(opPrefix,            boost::get(&Vertex_Properties::op,           graph));
     dp.property(nodeTPrefix,         boost::get(&Vertex_Properties::nodeType,     graph));
     dp.property(constantValuePrefix, boost::get(&Vertex_Properties::const_value,  graph));
     dp.property(widthPrefix,         boost::get(&Vertex_Properties::bitwidth,     graph));
     dp.property(BasicBlockPrefix,    boost::get(&Vertex_Properties::BBID,         graph));
+    dp.property("label",             boost::get(&Edge_Properties::arg_no,         graph));
     std::ifstream myfile (filename);
     boost::read_graphviz(myfile, graph, dp);
+    auto es = boost::edges(graph);
+    for(auto eit = es.first ; eit != es.second; ++eit){
+        auto def_eit = *eit;
+        std::cout << graph[def_eit].arg_no<<'\n'; //prints nothing?
+    }
 }
