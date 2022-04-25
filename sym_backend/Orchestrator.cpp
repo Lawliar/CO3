32ad29c34bf52fe6c0fdf96f9a7c73d3a66325b3
Changming Liu <charley.ashbringer@gmail.com>
2022-04-24 21:34:20 -0400

    remove nop

--- sym_backend/Orchestrator.cpp
+++ sym_backend/Orchestrator.cpp
@@ -10,7 +10,6 @@ Orchestrator::Orchestrator(std::string cfg_filename, std::string ddg_filename, s
     cfg.readGraphViz(cfg_filename);
     ddg.readGraphViz(ddg_filename);
     sp = initSerialPort(sp_port.c_str(), baud_rate);
-    __asm__("nop");
 }
 Orchestrator::~Orchestrator() {
     freeSerialPort(sp);
