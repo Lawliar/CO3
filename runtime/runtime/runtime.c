7aaf000e2724f914e6aa87a0581135ed9304025d
Changming Liu <charley.ashbringer@gmail.com>
2022-04-27 14:23:41 -0400

    add template for runtime lib

---
+++ runtime/runtime/runtime.c
@@ -0,0 +1,22 @@
+#include"stdint.h"
+void _spear_report1(uint32_t userID, char * arg1){
+
+}
+void _spear_report2(uint32_t userID, char * arg1,char * arg2){
+    
+}
+void _spear_report3(uint32_t userID, char * arg1,char * arg2,char * arg3){
+    
+}
+void _spear_report4(uint32_t userID, char * arg1,char * arg2,char * arg3,char * arg4){
+    
+}
+void _sym_notify_call(uint32_t id){
+
+}
+void _sym_notify_return(uint32_t id){
+
+}
+void _sym_notify_basic_block(uint32_t id){
+    
+}
