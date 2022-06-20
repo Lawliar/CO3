d3bb3fd3f0b162b4cc97a0ab78c475dacbddde86
Changming Liu <charley.ashbringer@gmail.com>
2022-06-20 19:14:04 -0400

    try to port CROMU_00001

---
+++ firmware/STMfirmware/SpearCROMU_00001/Core/Inc/ProtocolConfig.h
@@ -0,0 +1,16 @@
+/*
+ * ProtocolConfig.h
+ *
+ *  Created on: May 2, 2022
+ *      Author: alejandro
+ */
+
+#ifndef PROTOCOLCONFIG_H_
+#define PROTOCOLCONFIG_H_
+
+#define MAX_BUFFER_INPUT 128
+#define MAX_BUFFER_OUTPUT 128
+#define MAX_USB_FRAME 64
+#define MAX_USB_FUNCTIONS 8
+
+#endif /* PROTOCOLCONFIG_H_ */
---
+++ firmware/STMfirmware/SpearCROMU_00001/null.d
@@ -0,0 +1,3 @@
+/home/lcm/github/spear/spear-code/firmware/STMfirmware/SpearCROMU_00001/build/SpearCROMU_00001/Target/UninstrumentedSrc/libcgc.o \
+  /home/lcm/github/spear/spear-code/firmware/STMfirmware/SpearCROMU_00001/build/SpearCROMU_00001/Target/UninstrumentedSrc/libcgc.o: \
+  /dev/null
