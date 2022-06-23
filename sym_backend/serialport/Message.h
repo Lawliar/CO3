87ae8e96d7b863a6390c7552c6e8dfaee8861150
Changming Liu <charley.ashbringer@gmail.com>
2022-06-22 22:32:01 -0400

    add print for each Msg

--- sym_backend/serialport/Message.h
+++ sym_backend/serialport/Message.h
@@ -6,6 +6,7 @@
 #define SYMBACKEND_MESSAGE_H
 
 #include "stdint.h"
+#include <sstream>
 
 class Message {
 public:
@@ -52,6 +53,11 @@ class RuntimeBoolValueMessage: public SymSourceMessage{
 public:
     RuntimeBoolValueMessage(uint16_t symid,bool value):SymSourceMessage(BoolRuntimeValMsg, symid), value(value){};
     bool value;
+    std::string Str() {
+        std::ostringstream s;
+        s<<"RuntimeBoolValue:symid"<<static_cast<unsigned>(symid)<<",value:"<<value;
+        return s.str();
+    }
 };
 
 class PhiValueMessage: public ControlMessgaes{
@@ -59,6 +65,11 @@ public:
     PhiValueMessage(uint16_t symid,uint8_t value):ControlMessgaes(PhiMsg), symid(symid), value(value){};
     uint16_t symid;
     uint8_t value;
+    std::string Str() {
+        std::ostringstream s;
+        s<<"PhiValue:symid"<<static_cast<unsigned>(symid)<<",value:"<<static_cast<unsigned>(value);
+        return s.str();
+    }
 };
 
 class RuntimeIntValueMessage: public SymSourceMessage{
@@ -66,24 +77,44 @@ public:
     RuntimeIntValueMessage(uint16_t symid, uint8_t width, int32_t value):SymSourceMessage(IntRuntimeValMsg, symid),width(width), value(value){};
     uint8_t width;
     int32_t value;
+    std::string Str() {
+        std::ostringstream s;
+        s<<"RuntimeIntMsg:symid"<<static_cast<unsigned>(symid)<<",width:"<<width<<",runtime:"<<static_cast<unsigned>(value);
+        return s.str();
+    }
 };
 
 class RuntimeFloatValueMessage: public SymSourceMessage{
 public:
     RuntimeFloatValueMessage(uint16_t symid,float value):SymSourceMessage(FloatRuntimeValMsg, symid), value(value){};
     float value;
+    std::string Str() {
+        std::ostringstream s;
+        s<<"RuntimeFloatMsg:symid"<<static_cast<unsigned>(symid)<<",runtime:"<<value;
+        return s.str();
+    }
 };
 
 class RuntimeDoubleValueMessage: public SymSourceMessage{
 public:
     RuntimeDoubleValueMessage(uint16_t symid,double value):SymSourceMessage(DoubleRuntimeValMsg, symid), value(value){};
     double value;
+    std::string Str() {
+        std::ostringstream s;
+        s<<"RuntimeDoubleMsg:symid"<<static_cast<unsigned>(symid)<<",runtime:"<<value;
+        return s.str();
+    }
 };
 
 class PushConstraintMessage: public SymSinkMessage{
 public:
     PushConstraintMessage(uint16_t symID, bool runtimeVal):SymSinkMessage(ConstraintRuntimeValMsg, symID), runtimeVal(runtimeVal){};
     bool runtimeVal;
+    std::string Str(){
+        std::ostringstream s;
+        s<<"PushConstraint:symid"<<static_cast<unsigned>(symid)<<",runtime:"<<runtimeVal;
+        return s.str();
+    }
 };
 
 class MemCpyMessage: public SymSinkMessage{
@@ -92,6 +123,11 @@ public:
     uint32_t dst_ptr;
     uint32_t src_ptr;
     uint32_t length;
+    std::string Str(){
+        std::ostringstream s;
+        s<<"MemCpyMsg:symid"<<static_cast<unsigned>(symid)<<",dest:"<<std::hex<<dst_ptr<<",src:"<<std::hex<<src_ptr<<",len:"<<length;
+        return s.str();
+    }
 };
 
 class MemSetMessage: public SymSinkMessage{
@@ -99,6 +135,11 @@ public:
     MemSetMessage(uint16_t symID, uint32_t ptr, uint32_t length): SymSinkMessage(MemsetRuntimeMsg,symID), ptr(ptr),length(length){};
     uint32_t ptr;
     uint16_t length;
+    std::string Str(){
+        std::ostringstream s;
+        s<<"MemSetMsg:symid"<<static_cast<unsigned>(symid)<<",dest:"<<std::hex<<ptr<<",len:"<<length;
+        return s.str();
+    }
 };
 
 class MemMoveMessage: public SymSinkMessage{
@@ -107,42 +148,77 @@ public:
     uint32_t dst_ptr;
     uint32_t src_ptr;
     uint32_t length;
+    std::string Str(){
+        std::ostringstream s;
+        s<<"MemMoveMsg:symid"<<static_cast<unsigned>(symid)<<",dest:"<<std::hex<<dst_ptr<<",src:"<<std::hex<<src_ptr<<",len:"<<length;
+        return s.str();
+    }
 };
 
 class ReadMemMessage: public SymSourceMessage{
 public:
     ReadMemMessage(uint16_t symID, uint32_t ptr): SymSourceMessage(ReadMemRuntimeMsg, symID), ptr(ptr){}
     uint32_t ptr;
+    std::string Str(){
+        std::ostringstream s;
+        s<<"ReadMemMsg:symid"<<static_cast<unsigned>(symid)<<",mem:"<<std::hex<<ptr;
+        return s.str();
+    }
 };
 
 class WriteMemMessage: public SymSinkMessage{
 public:
     WriteMemMessage(uint16_t symID, uint32_t ptr): SymSinkMessage(WriteMemRuntimeMsg, symID), ptr(ptr){}
     uint32_t ptr;
+    std::string Str(){
+        std::ostringstream s;
+        s<<"WriteMemMsg:symid"<<static_cast<unsigned>(symid)<<",mem:"<< std::hex <<ptr;
+        return s.str();
+    }
 };
 
 class NotifyCallMessage: public ControlMessgaes{
 public:
     NotifyCallMessage(uint8_t id): ControlMessgaes(CallMsg), id(id){};
     uint8_t id;
+    std::string Str(){
+        std::ostringstream s;
+        s<<"NotifyCallMsg:"<<static_cast<unsigned>(id);
+        return s.str();
+    }
 };
 
 class NotifyFuncMessage: public ControlMessgaes{
 public:
     NotifyFuncMessage(uint8_t id): ControlMessgaes(CallMsg), id(id){};
     uint8_t id;
+    std::string Str(){
+        std::ostringstream s;
+        s<<"NotifyFuncMsg:"<<static_cast<unsigned>(id);
+        return s.str();
+    }
 };
 
 class NotifyRetMessage: public ControlMessgaes{
 public:
     NotifyRetMessage(uint8_t id): ControlMessgaes(RetMsg), id(id){};
     uint8_t id;
+    std::string Str(){
+        std::ostringstream s;
+        s<<"NotifyRetMsg:"<<static_cast<unsigned>(id);
+        return s.str();
+    }
 };
 
 class NotifyBasicBlockMessage: public ControlMessgaes{
 public:
     NotifyBasicBlockMessage(uint16_t id): ControlMessgaes(BasicBlockMsg), id(id){};
     uint16_t id;
+    std::string Str(){
+        std::ostringstream s;
+        s<<"NotifyBBMsg:"<<static_cast<unsigned>(id);
+        return s.str();
+    }
 };
 
 #endif //SYMBACKEND_MESSAGE_H
