f4dafdf26594ef337c05357b8b044e7dfd5b0d5d
Changming Liu <charley.ashbringer@gmail.com>
2022-07-14 16:32:10 -0400

    receive arbitrary number of bytes from the MCU

--- USBtest/serialEcho.py
+++ USBtest/serialEcho.py
@@ -1,20 +1,36 @@
 #!/usr/bin/env python3
 import serial
 from struct import *
+import sys,os
+
+args = sys.argv
+if(len(args) == 2 ):
+    assert(os.path.exists(args[1]))
+
+input_data = b'' 
+with open(args[1],"rb") as rfile:
+    input_data = rfile.read()
 
 ser = serial.Serial('/dev/ttyACM1')
 ser.timeout = 2
 print('timeout: {}'.format(ser.timeout))
 
-data = b'hello serial!'
+if(len(input_data) == 0):
+    data = b'hello serial!'
+else:
+    data = input_data
 inputLen = len(data)
 data =  pack('B', inputLen) + data
 
 ser.write(data)
-frameNum = 8096
-r = ser.read(frameNum)
+frameNum = 1024
+r = b''
+while True:
+    tmp_r = ser.read(frameNum)
+    r += tmp_r
+    if(len(tmp_r) != frameNum):
+        break
 print(len(r))
-print(r)
 f=open("data",'wb')
 f.write(r)
 f.close()
