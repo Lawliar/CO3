c75aac2ac73e0cb64c186267ea6a32b7574b1b14
Changming Liu <charley.ashbringer@gmail.com>
2022-08-02 20:21:11 -0400

    add code coverage and patched program for symcc

--- code_coverage/main.py
+++ code_coverage/main.py
@@ -5,22 +5,25 @@ from IPython import embed
 from tqdm import tqdm
 import subprocess
 
-input_dir = "/home/lcm/github/spear/spear-code/code_coverage/CROMU_00001"
+input_dir = "/home/lcm/github/spear/spear-code/code_coverage/CROMU_00002"
 build_dir = os.path.join(input_dir,"build")
-executable = os.path.join(build_dir,"cromu_00001")
-symcc_input = "/home/lcm/github/spear/symcc/shared_volume/CROMU_00001/build/orig_input"
-spear_input = "/home/lcm/github/spear/spear-code/firmware/STMfirmware/SpearCROMU_00001/intermediate_results/orig_input"
-def checkMake():
-    assert(os.path.exists(os.path.join(input_dir,"makefile")))
-    if( not os.path.exists(os.path.join(input_dir,"build")) ):
-        os.chdir(input_dir)
-        os.system("make")
-        os.chdir("..")
+executable = os.path.join(build_dir,"out")
+symcc_input = "/home/lcm/github/spear/symcc/shared_volume/CROMU_00002/output"
+spear_input = "/home/lcm/github/spear/spear-code/firmware/STMfirmware/SpearCROMU_00002/intermediate_results/output"
+
+assert(os.path.exists(input_dir))
+assert(os.path.exists(build_dir))
+assert(os.path.exists(executable))
+assert(os.path.exists(symcc_input))
+assert(os.path.exists(spear_input))
 
 
-def runOne(input_dir):
+
+def runOne(input_dir, just_one = False):
     ret = set()
     for eachfile in tqdm(os.listdir(input_dir)):
+        if(just_one and eachfile != "000000"):
+            continue
         input_file = os.path.join(input_dir, eachfile)
         result_file = os.path.join(build_dir,"coverage")
         if(os.path.exists(result_file)):
@@ -32,7 +35,8 @@ def runOne(input_dir):
         assert(exit_status == 0)
         p2 = subprocess.Popen([executable], stdin=p1.stdout)
         exit_status = p2.wait()
-        assert(exit_status == 0)
+        #embed()
+        #assert(exit_status == 0)
 
         assert(os.path.exists(result_file))
         with open(result_file,"r") as rfile:
@@ -43,7 +47,7 @@ def runOne(input_dir):
         os.system("rm coverage")
     return ret
 def runTest():
-    os.chdir(os.path.join(input_dir,"build"))
+    os.chdir(build_dir)
     symcc_bb = runOne(symcc_input)
     spear_bb = runOne(spear_input)
     print("symcc covered BB:{}\n".format(len(symcc_bb)))
@@ -51,7 +55,7 @@ def runTest():
     embed()
         
 def main():
-    checkMake()
+    #checkMake()
     runTest()
 if __name__ == '__main__':
     main()
---
+++ symcc_benchmark/shared_volume/CROMU_00001/Src/libcgc.c
@@ -0,0 +1,103 @@
+/*
+ * Copyright (C) Narf Industries <info@narfindustries.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+//#include <sys/mman.h>
+#include "libcgc.h"
+#include "stdlib.h" // for free and malloc
+#include "string.h"
+// lame so we don't have to include over-riding .h files
+#define MAP_ANONYMOUS 0x20
+#define MAP_PRIVATE 0x02
+// #define PROT_READ 0x01
+// #define PROT_WRITE 0x02
+
+#define _SC_PAGESIZE 128
+// todo prot_exec
+
+
+
+/*
+void _terminate(unsigned int status){
+	_exit(1);
+}*/
+
+int transmit(int fd, const void *buf, size_t_cgc count, size_t_cgc *tx_bytes){
+	//ssize_t_cgc txb = write(fd, buf, count);
+	//if(txb < 0)
+	//	return EFAULT;
+	//*tx_bytes = (size_t_cgc) txb;
+	//return 0;
+	return 0;
+}
+
+int receive_cgc( int fd, void *buf, size_t count, size_t *rx_bytes){
+		unsigned s = read(fd, buf, count);
+        if(s <= 0)
+                return EFAULT;
+        *rx_bytes = (size_t) s;
+        return 0;
+}
+
+
+int allocate_cgc(size_t_cgc length, int is_X, void **addr){
+	size_t_cgc ps = _SC_PAGESIZE;
+	if(length % ps != 0)
+		length += (ps - (length % ps));
+
+
+	//void *c = mmap(NULL, length, PROT_READ | PROT_WRITE,  MAP_ANONYMOUS|MAP_PRIVATE, -1, NULL);
+	
+	void *c = malloc(length);
+	if(c != NULL && c != (void *) -1){
+		*addr = c;
+		return 0;
+	}
+	// todo see if we can bring errno in ex = errno
+	int ex = EFAULT;
+	return ex;
+
+}
+
+int deallocate_cgc(void *addr, size_t_cgc length){
+	// warning! length not used!
+
+	size_t_cgc ps = _SC_PAGESIZE;//sysconf(_SC_PAGESIZE);
+	if(length % ps != 0)
+		length += (ps - (length % ps));
+
+	free(addr);
+	return 0;
+	//return munmap(addr, length);
+}
+
+// int random_cgc(void *buf, size_t_cgc count, size_t_cgc *rnd_bytes){
+// 	size_t_cgc read_sz = 0;
+// 	// 0x00 is for rdonly
+// 	int rndfd = open("/dev/urandom", 0x00);
+	
+// 	ssize_t_cgc res = read(rndfd, (char *) buf, count);
+// 	if(res < 0)
+// 		return EFAULT;
+// 	*rnd_bytes = (size_t_cgc) res;
+// 	return 0;
+
+// }
