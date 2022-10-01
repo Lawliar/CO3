4772939197013e6df23028948ff6384102b1b453
Changming Liu <charley.ashbringer@gmail.com>
2022-09-30 20:42:02 -0400

    make midi dma work for spear

---
+++ firmware/STMfirmware/SpearminiDMA/Target/Inc/ConfigFuzzing.h
@@ -0,0 +1,33 @@
+/*
+ * ConfigFuzzing.h
+ *
+ *  Created on: Jan 26, 2022
+ *      Author: alejandro
+ */
+
+#ifndef FUZZING_CONFIG_CONFIGFUZZING_H_
+#define FUZZING_CONFIG_CONFIGFUZZING_H_
+
+
+
+/* BITMAP defines */
+#define SIZE64  (1024 * 64)
+#define SIZE32  (1024 * 32)
+#define SIZE16  (1024 * 16)
+#define SIZE8  (1024 * 8)
+#define SIZE4  (1024 * 4)
+#define MAX_BUFFER_INPUT  (2048)       // 2 kb
+#define AFL_BITMAP_SIZE   SIZE8
+
+
+
+#define FUZZING_CYCLES_IN_PROCESS 100   //number of cycles that we will use the same task before deleting and spawning a new one
+#define McuASAN_MAX_NUMBER_ALLOCS 10
+#define TARGET_TIMEOUT 20
+
+#define  PERSISTENT_MODE 1
+#define DUALCOREFUZZ 0
+
+
+
+#endif /* FUZZING_CONFIG_CONFIGFUZZING_H_ */
---
+++ firmware/STMfirmware/SpearminiDMA/Target/Inc/midi_main.h
@@ -0,0 +1,13 @@
+/*
+ * midi_main.h
+ *
+ *  Created on: Jan 30, 2022
+ *      Author: alejandro
+ */
+
+#ifndef TARGET_MMMIDI_INC_MIDI_MAIN_H_
+#define TARGET_MMMIDI_INC_MIDI_MAIN_H_
+
+int main_midi(void);
+
+#endif /* TARGET_MMMIDI_INC_MIDI_MAIN_H_ */
