239fecdc6f7f771bfdae301a21f4020457d07fae
Changming Liu <charley.ashbringer@gmail.com>
2023-10-07 20:46:36 -0400

    add usart soft support to the protocol

--- firmware/STMfirmware/Common/Protocol/config/ProtocolConfigTemplate.h
+++ firmware/STMfirmware/Common/Protocol/config/ProtocolConfigTemplate.h
@@ -11,9 +11,14 @@
 #ifndef PROTOCOLCONFIG_H_
 #define PROTOCOLCONFIG_H_
 
-#define MAX_BUFFER_INPUT 128
+#define AFL_BUFFER_STARTING_POINT 4
+#define REPORTING_BUFFER_STARTING_POINT 1
+#define MAX_BUFFER_INPUT 1036
 #define MAX_BUFFER_OUTPUT 128
 #define MAX_USB_FRAME 64
 #define MAX_USB_FUNCTIONS 8
+
+// see if you wanna use serial port over usb
+//#define USE_SERIAL_OVER_USB
 
 #endif /* PROTOCOLCONFIG_H_ */
--- firmware/STMfirmware/Common/Protocol/protocol.c
+++ firmware/STMfirmware/Common/Protocol/protocol.c
@@ -10,7 +10,15 @@
 #include "task.h"
 #include "protocol.h"
 #include "stdint.h"
+#include "main.h"
+
+#ifdef USE_SERIAL_OVER_USB
+extern UART_HandleTypeDef huart2; //PUT the usart that you wanna use here
+UART_HandleTypeDef* co3_huart = &huart2;
+uint8_t co3_usart_input_buffer[MAX_BUFFER_INPUT];
+#else
 #include "usbd_cdc_if.h"
+#endif
 
 
 extern Symex_t AFLfuzzer;
@@ -46,7 +54,7 @@ void notifyTXfinish()
 	//notify the target to continue execution
 	if (notiTarget == NOTI_TARGET)
 	{
-	xTaskNotifyIndexedFromISR(AFLfuzzer.xTaskTarget,
+		xTaskNotifyIndexedFromISR(AFLfuzzer.xTaskTarget,
   	  	    				1, //index
   							1, //value = 1 data TX complete
   							eSetBits,
@@ -76,11 +84,66 @@ void TransmitPack(void)
 		printf("TX buffer f. num: %d\n", AFLfuzzer.txTotalFunctions);
 #endif
 		AFLfuzzer.txbuffer[0]= AFLfuzzer.txCurrentIndex; //set the total length of the payload without considering size itself
+#ifdef USE_SERIAL_OVER_USB
+		HAL_UART_Transmit(co3_huart,AFLfuzzer.txbuffer,AFLfuzzer.txCurrentIndex,HAL_MAX_DELAY);
+		AFLfuzzer.txCurrentIndex=REPORTING_BUFFER_STARTING_POINT;  //we reserve the first byte for size
+		//clean the tx buffer
+		for(uint8_t j = 0; j<MAX_USB_FRAME; j++ )
+		{
+			AFLfuzzer.txbuffer[j]=0;
+		}
+		AFLfuzzer.txTotalFunctions=0;
+		// wake up the target who is waiting on the transmission
+		BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+		xTaskNotifyIndexedFromISR(AFLfuzzer.xTaskTarget,
+		  	  	    				1, //index
+		  							1, //value = 1 data TX complete
+		  							eSetBits,
+		  							&xHigherPriorityTaskWoken);
+		//sanity check, since only the monitor uses usart to transmit this.
+		if(xTaskGetCurrentTaskHandle() != AFLfuzzer.xTaskMonitor){
+			while(1){}
+		}
+
+#else
 		CDC_Transmit_FS(AFLfuzzer.txbuffer, AFLfuzzer.txCurrentIndex); //transmit data
+#endif
 	}
 
 }
 
+
+void SerialReceiveInput(uint8_t* Buf, uint32_t *Len)
+{
+
+	  //union ubytes_t auxbytes;
+	  uint32_t u32Tocopy;
+	  uint8_t error;
+	  if(AFLfuzzer.inputLength == 0)
+	  {
+	      HAL_UART_Receive(co3_huart, (uint8_t * )&AFLfuzzer.inputLength, sizeof(uint32_t), HAL_MAX_DELAY );
+	      AFLfuzzer.inputLengthpadded  = (AFLfuzzer.inputLength + 3) & ~0x03;
+	      if((AFLfuzzer.inputLengthpadded)> MAX_BUFFER_INPUT)
+	      {
+        	  error = 1;
+          }
+      }
+
+      if( AFLfuzzer.inputLengthpadded && (error == 0) )
+      {
+	    	 u32Tocopy = (AFLfuzzer.inputLengthpadded) - AFLfuzzer.inputAFL.u32available;
+	    	 HAL_UART_Receive(co3_huart,co3_usart_input_buffer,u32Tocopy,HAL_MAX_DELAY);
+	    	 RingCopy(&AFLfuzzer.inputAFL, (uint8_t * )&AFLfuzzer.inputLength, AFL_BUFFER_STARTING_POINT);
+	    	 RingCopy(&AFLfuzzer.inputAFL, Buf, u32Tocopy);
+
+
+	  	     if( AFLfuzzer.inputLengthpadded == AFLfuzzer.inputAFL.u32available)
+	  	     {
+                 AFLfuzzer.inputAFL.u32availablenopad = AFLfuzzer.inputLength;
+	  	    	 AFLfuzzer.bRXcomplete = 1;
+	  	     }
+      }
+}
 
 void FuzzingInputHandler(uint8_t* Buf, uint32_t *Len)
 {
