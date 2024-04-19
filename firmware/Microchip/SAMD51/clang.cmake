14b8d573100fca2b213fcddfd2867afceeac3977
Changming Liu <charley.ashbringer@gmail.com>
2024-04-19 16:03:29 -0400

    license for microchip firmware

--- firmware/Microchip/SAMD51/clang.cmake
+++ firmware/Microchip/SAMD51/clang.cmake
@@ -1,3 +1,18 @@
+# This file is part of CO3.
+#
+# CO3 is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# CO3 is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# CO3. If not, see <https://www.gnu.org/licenses/>.
+
+
 # Call Cmake from the 'build' subfolder with the command below.
 # For using Make:
 # cmake -DCMAKE_MAKE_PROGRAM=make.exe -DCMAKE_TOOLCHAIN_FILE="arm-none-eabi-gcc.cmake" -G "Unix Makefiles" ..
